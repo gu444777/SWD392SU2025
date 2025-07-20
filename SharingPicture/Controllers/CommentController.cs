f7cc63df088788dd38bb1456fead5140e0b03fe0
Dat Dinh Tien <134479105+dinhtiendat2012@users.noreply.github.com>
2025-07-20 23:34:06 +0700

    Add files via upload

---
+++ SharingPicture/Controllers/CommentController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SharingPicture.Services;
+using SharingPicture.Models;
+
+namespace SharingPicture.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CommentController : ControllerBase
+    {
+        private readonly ICommentService _commentService;
+
+        public CommentController(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+
+        [HttpPost("add")]
+        public async Task<IActionResult> AddComment([FromBody] AddCommentRequest request)
+        {
+            var comment = await _commentService.AddCommentAsync(request.Content, request.PictureId, request.UserId);
+            return Ok(comment);
+        }
+    }
+
+    public class AddCommentRequest
+    {
+        public string Content { get; set; } = null!;
+        public int PictureId { get; set; }
+        public int UserId { get; set; }
+    }
+}
