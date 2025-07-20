eba3849317a1e4fbfc740297807aed354c8295e8
Dat Dinh Tien <134479105+dinhtiendat2012@users.noreply.github.com>
2025-07-20 23:35:08 +0700

    Add files via upload

---
+++ SharingPicture/Services/CommentService.cs
@@ -0,0 +1,27 @@
+﻿using SharingPicture.Models;
+using SharingPicture.Repositories;
+
+namespace SharingPicture.Services
+{
+    public class CommentService : ICommentService
+    {
+        private readonly ICommentRepository _commentRepository;
+
+        public CommentService(ICommentRepository commentRepository)
+        {
+            _commentRepository = commentRepository;
+        }
+
+        public async Task<Comment> AddCommentAsync(string content, int pictureId, int userId)
+        {
+            var comment = new Comment
+            {
+                Content = content,
+                PictureId = pictureId,
+                UserId = userId
+            };
+
+            return await _commentRepository.AddCommentAsync(comment);
+        }
+    }
+}
---
+++ SharingPicture/Services/ICommentService.cs
@@ -0,0 +1,9 @@
+﻿using SharingPicture.Models;
+
+namespace SharingPicture.Services
+{
+    public interface ICommentService
+    {
+        Task<Comment> AddCommentAsync(string content, int pictureId, int userId);
+    }
+}
